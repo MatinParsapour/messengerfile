0eed626ce393daa1c6bd1e9d9f799a70da876ff5
Matin <matin.parsapour.iam@gmail.com>
2021-09-10 21:29:53 +0430

    create readfile method

--- src/main/java/readfile/Reading.java
+++ src/main/java/readfile/Reading.java
@@ -1,4 +1,19 @@
 package readfile;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class Reading {
+    private static final String FILE_NAME = "List.txt";
+
+    public static void main(String[] args) throws IOException {
+        readFile();
+    }
+    public static void readFile() throws IOException {
+        RandomAccessFile random = new RandomAccessFile(FILE_NAME,"r");
+        while (random.getFilePointer() < random.length()){
+            System.out.println(random.readLine());
+        }
+        random.close();
+    }
 }
